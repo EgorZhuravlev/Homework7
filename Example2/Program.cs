307f4541c4528c2f65d2354a5e2516d0c61e6f31
Egor Zhuravlev <zhuravlevegor@gmail.com>
2022-12-13 20:58:23 +0300

    Example2 modified

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -12,16 +12,12 @@
 
 17 -> такого числа в массиве нет */
 
-bool isParsedX = int.TryParse(Console.ReadLine(), out int X);
-if (!isParsedX)
-{
-    Console.WriteLine("Данные ввели некорректно");
-}
 
+int number = GetNumerToFind();
 int[,] array = CreateRandom2DArray(3,4);
 Print2DArray(array);
-int[] position = FoundNumber(array, X);
-PrintPosition(position, X);
+int[] position = FoundNumber(array, number);
+PrintPosition(position, number);
 
 
 int[,] CreateRandom2DArray(int rows, int columns)
@@ -40,14 +36,14 @@ int[,] CreateRandom2DArray(int rows, int columns)
     return array;
 }
 
-int[] FoundNumber(int[,] array, int X)
+int[] FoundNumber(int[,] array, int num)
 {
     int[] position = new int[2];
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            if (X == array[i,j])
+            if (num == array[i,j])
             {
                 position[0] = i + 1;
                 position[1] = j + 1;
@@ -73,10 +69,20 @@ void Print2DArray(int[,] array)
     }
 }
 
-void PrintPosition(int[] position, int X)
+void PrintPosition(int[] position, int num)
 {
     System.Console.WriteLine();
-    if (position[0] > 0 && position[1] > 0) System.Console.WriteLine($"Число {X} находится в {position[0]}-й строке, {position[1]}-м столбце");
-    else System.Console.WriteLine($"Число {X} отсутствует в заданном массиве");
+    if (position[0] > 0 && position[1] > 0) System.Console.WriteLine($"Число {num} находится в {position[0]}-й строке, {position[1]}-м столбце");
+    else System.Console.WriteLine($"Число {num} отсутствует в заданном массиве");
     System.Console.WriteLine();
+}
+
+int GetNumerToFind()
+{
+    bool isParsedX = int.TryParse(Console.ReadLine(), out int num);
+    if (!isParsedX)
+    {
+    Console.WriteLine("Данные ввели некорректно");
+    }
+    return num;
 }
